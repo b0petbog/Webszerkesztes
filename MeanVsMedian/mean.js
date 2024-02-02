d76f75fd093f43e75b2d12b45d60e66f165cae20
2020b-Pethő Boglárka <74898888+b0petbog@users.noreply.github.com>
2024-02-02 09:23:23 +0100

    Add files via upload

---
+++ MeanVsMedian/mean.js
@@ -0,0 +1,29 @@
+let tomb = [1, 1, 2, 3, 3, 3, 4, 4, 5];
+
+function median(numbers) 
+{
+    var median = 0;
+    numbers.sort();
+    if (numbers.length % 2 == 0)
+    {
+        median = (numbers[numbers.length / 2 - 1] + numbers[numbers.length / 2]) / 2;
+    }
+    else
+    {
+        median = numbers[(numbers.length - 1) / 2];
+    }
+    return median;
+}
+
+function mean(numbers) 
+{
+    var mean = 0;
+    for (let i = 0; i < numbers.length; i += 1) 
+    {
+        mean += numbers[i];
+    }
+    return mean / numbers.length;
+}
+
+console.log(median(tomb));
+console.log(mean(tomb));
