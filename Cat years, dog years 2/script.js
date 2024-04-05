6bdd09d896cde529cd6fa21f29721811f50a204d
2020b-Pethő Boglárka <74898888+b0petbog@users.noreply.github.com>
2024-04-05 09:32:54 +0200

    Add files via upload

---
+++ Cat years, dog years 2/script.js
@@ -0,0 +1,45 @@
+function ownedCat(catYears)
+{
+    let humanYears = 0;
+    let tempCatYrs;
+    while(tempCatYrs<catYears)
+    {
+        if(humanYears==0)
+        {
+            tempCatYrs+=15;
+        }
+        else if(humanYears==1)
+        {
+            tempCatYrs+=9;
+        }
+        else
+        {
+            tempCatYrs+=3;
+        }
+        humanYears++;
+        return humanYears;
+    }
+}
+
+function ownedDog(dogYears)
+{
+    let humanYears = 0;
+    let tempDogYrs;
+    while(tempDogYrs<dogYears)
+    {
+        if(humanYears==0)
+        {
+            tempDogYrs+=15;
+        }
+        else if(humanYears==1)
+        {
+            tempDogYrs+=9;
+        }
+        else
+        {
+            tempDogYrs+=5;
+        }
+        humanYears++;
+        return humanYears;
+    }
+}
