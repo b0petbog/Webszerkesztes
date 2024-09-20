d1d2670ec904fbdd23e3cd0d0a5ec14f47f57fac
2020b - Pethő Boglárka <74898888+b0petbog@users.noreply.github.com>
2024-09-20 14:21:10 +0200

    Add files via upload

--- Codewars/script.js
+++ Codewars/script.js
@@ -1,54 +1,31 @@
-
-// Összesített
 async function GetUser(username) {
     let user = null;
     try {
         const request = await fetch(`https://www.codewars.com/api/v1/users/${username}`);
-
         if(!request.ok)
             throw new Error('REQUEST ERROR');
-
         const response = await request.json();
-
         if(response.success) {
             if(response.success === "false")
                 return { "error": "A felhasználó nem található" };
         }
-
-        //console.log(response);
-
         user = response;
     }
     catch(err) { console.error(err); }
-
     return user;
 }
 
 async function ShowAll() {
     const uName = document.getElementById('seachedname').value;
     const user = await GetUser(uName);
-
-    if(user.error)
-        document.querySelector('#container').innerHTML += `
-            <div class="result" id="hiba">
-                <p>Hiba</p>
-            </div>`;
-    
-    document.querySelector('#container').innerHTML += `
-        <div class="result" id="showallcard">
+    document.querySelector('#cards').innerHTML += `
+        <div id="showallcard">
             <p>Felhasználónév: ${user.username}</p>
-            <p>Teljes név</p>
-            <p>Klán</p>
-            <p>Rank</p>
-            <p>Pontszám</p>
-                <td></td>
-                <td>${user.name}</td>
-                <td>${user.clan}</td>
-                <td>${user.ranks.overall.rank}</td>
-                <td>${user.ranks.overall.score}</td>
-            </tr>
-        </table>
-    </div>`;
+            <p>Teljes név: ${user.name}</p>
+            <p>Klán: ${user.clan}</p>
+            <p>Rank: ${user.ranks.overall.rank}</p>
+            <p>Pontszám: ${user.ranks.overall.score}</p>
+        </div>`;
 }
 
 function getValueOfObject(obj, k) {
@@ -60,25 +37,19 @@ function getValueOfObject(obj, k) {
 
 async function ShowByLanguages() {
     const uName = document.getElementById('seachedname').value;
-    let languages = getValueOfObject(await GetUser(uName), 'ranks'), output = `<div class="result" id="languages">
-    <table>
-        <caption>${uName}</caption>
-        <tr>
-            <th>Nyelv</th>
-            <th>Rank</th>
-            <th>Pontszám</th>
-        </tr>`;
-    console.log(languages);
-    
+    let languages = getValueOfObject(await GetUser(uName), 'ranks'), output = `<div id="languagescard">
+        <p>Felhasználónév: ${uName}</p>`;
+            
     for(let [key, value] of Object.entries(languages)) {
        output += `
-            <tr>
-                <td>${key}</td>
-                <td>${value.rank}</td>
-                <td>${value.score}</td>
-            </tr>`;
+       <p>Nyelv: ${key}</p>
+       <p>Rank: ${value.rank}</p>
+       <p>Pontszám: ${value.score}</p>`;
     }
-    
-    output += `</table></div>`;
-    document.querySelector('#container').innerHTML += output;
+    output += `</div>`;
+    document.querySelector('#cards').innerHTML += output;
+}
+
+function SetBackSite() {
+    document.querySelector('#cards').innerHTML = ``;
 }
