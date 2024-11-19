4ddc6f15aa642fae5fd9e4819556941361a4eba7
2020b - Kószó Liliána <74898690+b0koslil@users.noreply.github.com>
2024-11-19 18:54:37 +0100

    Update admin.js

--- feladat/routes/admin.js
+++ feladat/routes/admin.js
@@ -9,11 +9,28 @@ const outgoingsFilePath = path.join(__dirname, 'public', 'data', 'outgoings.json
 // Helper function to save outgoing to file
 const saveOutgoing = (outgoing) => {
   fs.readFile(outgoingsFilePath, (err, data) => {
-    let outgoings = []
+    let outgoings = [];
     if (!err) {
-        outgoings = JSON.parse(data);  // Parse existing outgoings
+      outgoings = JSON.parse(data);  // Parse existing outgoings
     }
     outgoings.push(outgoing); // Add the new outgoing
+    fs.writeFile(outgoingsFilePath, JSON.stringify(outgoings, null, 2), (err) => {
+      if (err) {
+        console.log(err);
+      }
+    })
+  })
+}
+
+// Helper function to modify outgoing to file
+const modifyOutgoing = (outgoing) => {
+  fs.readFile(outgoingsFilePath, (err, data) => {
+    let outgoings = [];
+    if (!err) {
+      outgoings = JSON.parse(data);
+    }
+    const index = outgoings.findIndex(item => item.title === outgoing.title);
+    outgoings[index].price = outgoing.price
     fs.writeFile(outgoingsFilePath, JSON.stringify(outgoings, null, 2), (err) => {
       if (err) {
         console.log(err)
@@ -22,10 +39,28 @@ const saveOutgoing = (outgoing) => {
   })
 }
 
+// Helper function to remove outgoing to file
+const removeOutgoing = (outgoing) => {
+  fs.readFile(outgoingsFilePath, (err, data) => {
+    let outgoings = []
+    if (!err) {
+      outgoings = JSON.parse(data);
+    }
+    outgoings = outgoings.filter(item => item.title !== outgoing.title);
+    fs.writeFile(outgoingsFilePath, JSON.stringify(outgoings, null, 2), (err) => {
+      if (err) {
+        console.log(err)
+      }
+    })
+  })
+}
+
+// -----------------------------------------------------------------
+
 // /admin/add-outgoing => GET
 router.get('/add-outgoing', (req, res, next) => {
   res.render('add-outgoing', {
-    pageTitle: 'Add outgoing',
+    pageTitle: 'Kiadás hozzáadása',
     path: '/admin/add-outgoing'
   })
 })
@@ -34,6 +69,40 @@ router.get('/add-outgoing', (req, res, next) => {
 router.post('/add-outgoing', (req, res, next) => {
   const newOutgoing = { title: req.body.title, price: req.body.price }
   saveOutgoing(newOutgoing)
+  res.redirect('/')
+})
+
+// -------------------------------------------------------------------
+
+// /admin/remove-outgoing => GET
+router.get('/remove-outgoing', (req, res, next) => {
+  res.render('remove-outgoing', {
+    pageTitle: 'Kiadás törlése',
+    path: '/admin/remove-outgoing'
+  })
+})
+
+// /admin/remove-outgoing => POST
+router.post('/remove-outgoing', (req, res, next) => {
+  const outgoingToRemove = { title: req.body.title }
+  removeOutgoing(outgoingToRemove)
+  res.redirect('/')
+})
+
+// --------------------------------------------------------------------
+
+// /admin/modify-outgoing => GET
+router.get('/modify-outgoing', (req, res, next) => {
+  res.render('modify-outgoing', {
+    pageTitle: 'Kiadás módosítása',
+    path: '/admin/modify-outgoing'
+  })
+})
+
+// /admin/modify-outgoing => POST
+router.post('/modify-outgoing', (req, res, next) => {
+  const newOutgoing = { title: req.body.title, price: req.body.price }
+  modifyOutgoing(newOutgoing)
   res.redirect('/')
 })
 
